771620e6a2f67a5a8ac4d57a767d59c33b384d4c
Kacper Wikieł <k.wikiel@gmail.com>
2017-02-08 16:13:03 +0100

    Dynamic data sourcing

--- hellowebapp/collection/views.py
+++ hellowebapp/collection/views.py
@@ -1,6 +1,10 @@
 from django.shortcuts import render
-
+from collection.models import Thing
 # Create your views here.
 
 def index(request):
-    return render(request, 'index.html')
+    # Dynamic variable yay :3
+    things = Thing.objects.all()
+    return render(request, 'index.html', {
+        'things': things,
+        })
