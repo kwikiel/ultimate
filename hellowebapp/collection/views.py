7a2d24784fc5b2d162b80961340b2c60a35bf6d1
Kacper Wikieł <k.wikiel@gmail.com>
2017-02-09 19:28:57 +0100

    Desgin and cleanup

--- hellowebapp/collection/views.py
+++ hellowebapp/collection/views.py
@@ -1,5 +1,8 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from collection.forms import ThingForm
 from collection.models import Thing
+
+
 # Create your views here.
 
 def index(request):
@@ -7,4 +10,36 @@ def index(request):
     things = Thing.objects.all()
     return render(request, 'index.html', {
         'things': things,
+    })
+
+
+def thing_detail(request, slug):
+
+    thing = Thing.objects.get(slug=slug)
+
+    return render(request, 'things/thing_detail.html', {
+        'thing': thing
         })
+
+
+def edit_thing(request, slug):
+    #Taking object
+    thing = Thing.objects.get(slug=slug)
+
+    #Magic
+    form_class = ThingForm
+
+    if request.method == 'POST':
+
+        form = form_class(data=request.POST, instance=thing)
+        if form.is_valid():
+            form.save()
+            return redirect('thing_detail', slug=thing.slug)
+    else:
+        #What is this form class magic
+        form = form_class(instance=thing)
+
+    return render(request, 'things/edit_thing.html', {
+        'thing': thing,
+        'form': form,})
+
