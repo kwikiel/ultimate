177bb1d9ab070563f587117f68f8bd3f5052a679
Kacper Wikieł <random8902@gmail.com>
2017-02-09 19:37:02 +0100

    Thing detail

--- hellowebapp/hellowebapp/settings.py
+++ hellowebapp/hellowebapp/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = '9ac55q!d6_y+^kk&ef$$($-b_i-_bwg!hfq%qtw1v#pk2g+7@*'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['ancient-escarpment-65683.herokuapp.com']
+ALLOWED_HOSTS = ['ancient-escarpment-65683.herokuapp.com', '0.0.0.0']
 
 
 # Application definition
